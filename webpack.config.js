0c046dbdd1872e5948f846699ccb8fadccd4ad53
Fabien de Maestri <fabien.demaestri@gmail.com>
2016-06-09 01:22:12 +0200

    add webpack in project

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  entry : './src/app.js',
+  output : {
+    path : './dist',
+    filename : 'app.bundle.js'
+  },
+  module : {
+    loaders : [
+      {
+        test : /\.jsx?$/,
+        exclude : /node_modules/,
+        loader : 'babel-loader'
+      }
+    ]
+  }
+};
